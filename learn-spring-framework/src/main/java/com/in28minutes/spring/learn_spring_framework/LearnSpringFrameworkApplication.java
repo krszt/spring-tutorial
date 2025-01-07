1473d150109a6596600cbc144b186734b9fcb251
Kriszti <kriszti.karman@gmail.com>
2025-01-07 22:32:40 +0000

    Initial commit

---
+++ learn-spring-framework/src/main/java/com/in28minutes/spring/learn_spring_framework/LearnSpringFrameworkApplication.java
@@ -0,0 +1,19 @@
+package com.in28minutes.spring.learn_spring_framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.in28minutes.spring.learn_spring_framework.game.GameRunner;
+import com.in28minutes.spring.learn_spring_framework.game.MarioGame;
+
+@SpringBootApplication
+public class LearnSpringFrameworkApplication {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(LearnSpringFrameworkApplication.class, args);
+		MarioGame game = new MarioGame();
+		GameRunner runner = new GameRunner(game);
+		runner.run();
+	}
+
+}
